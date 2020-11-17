f7b9f9ca77c11129ed93023fd11efe93b38ef53e
Kim-Jaemin420 <jany240@naver.com>
2020-11-17 15:33:20 +0900

    cuisine 자바스크립트 1차 완성

---
+++ public/js/cuisine.js
@@ -0,0 +1,52 @@
+// State
+const ingredient = ['목살', '마늘', '설탕', '김치'];
+// import ingredient from './my-ref.js'
+
+let result = [];
+
+// DOMs
+const $containerWrap = document.querySelector('.container-wrap');
+
+const render = res => {
+  let html = '';
+
+  res.forEach(({ name, img, difficulty }) => {
+    html += `<div class="cuisine-container">
+    <figure class="cuisine">
+      <div class="img-wrapper">
+        <img src="${img}" alt="${name}" />
+        <span class="difficulty">${difficulty}</span>
+      </div>
+      <figcaption>${name}</figcaption>
+      <i class="fas fa-bookmark"></i>
+    </figure>
+  </div>`;
+  });
+
+  $containerWrap.innerHTML = html;
+};
+
+const fetchIng = async () => {
+  try {
+    const data = await fetch('/cuisine');
+    const res = await data.json();
+
+    res.forEach(cuisine => {
+      for (let i = 0; i < cuisine.ingredient.length; i++) {
+        // console.log(cuisine.ingredient[i]);
+        if (cuisine.ingredient.includes(ingredient[i])) {
+          result.push(cuisine);
+        }
+      }
+
+      const result1 = new Set(result);
+      console.log([...result1]);
+
+      render([...result1]);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+window.onload = fetchIng;
---
+++ public/js/my-ref.js
@@ -0,0 +1,4 @@
+
+const ingredient = ['목살', '마늘', '설탕', '김치'];
+
+export default ingredient;
