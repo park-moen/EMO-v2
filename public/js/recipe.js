62a0bb883914e8537701dcfa8bcc9c4a97aa407e
park moen <pmj6516@naver.com>
2020-11-17 15:34:23 +0900

    요리보기 js 1차 구현

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,21 +1,42 @@
 // status
 let foodList = [];
 // DOMs
-const backBtn = document.querySelector('.back-btn');
-const foodName = document.querySelector('.food-name');
-const lastSpan = document.querySelector('.food-title span:nth-child(3)');
-const superStuffList = document.querySelector('.super-stuff > ul');
-const subStuffList = document.querySelector('.sub-stuff > ul');
-const recipe = document.querySelector('.recipe');
+const $backBtn = document.querySelector('.back-btn');
+const $mainImage = document.querySelector('.main-image > img');
+const $foodName = document.querySelector('.food-name');
+const $lastSpan = document.querySelector('.food-title span:nth-child(3)');
+const $superStuffList = document.querySelector('.super-stuff > ul');
+const $recipe = document.querySelector('.recipe');
+// const $subStuffList = document.querySelector('.sub-stuff > ul');
 
 // function
-// const render = () => {
-//   foodList 
-// };
+const render = ({ ingredient, recipe }) => {
+  let stuffHtml = '';
+  let recipeHtml = '';
+
+  ingredient.forEach($list => {
+    stuffHtml += `<li>${$list}</li>`;
+  });
+  recipe.forEach($list => {
+    recipeHtml += `<li>${$list}</li>`;
+  });
+
+  $superStuffList.innerHTML = stuffHtml;
+  $recipe.innerHTML = recipeHtml;
+};
 
 const fetchFoodList = async () => {
-  const data = await fetch('/cuisine');
-  foodList = await data.json();
+  try {
+    const data = await fetch('/cuisine/6');
+    const res = await data.json();
+    foodList = res;
+    $foodName.textContent = foodList.name;
+    $lastSpan.textContent = foodList.difficulty;
+    $mainImage.setAttribute('src', `${foodList.img}`);
+    render(res);
+  } catch (e) {
+    console.error(`error: ${e}`);
+  }
 };
 // Event Binding
 window.onload = fetchFoodList;
