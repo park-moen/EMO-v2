5e0dcd24a5b2ceef1390f21e2952c490918e34a0
Kim-Jaemin420 <jany240@naver.com>
2020-11-19 05:45:03 +0900

    signup 페이지 1차 구현

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,79 @@
+// Reg
+const signReg = /^$|[A-Za-z0-9]{6,12}$/g;
+const nameReg = /^$|[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|*]{2,10}$/;
+
 // DOMs
-const $form = document.querySelector('form');
 const $userId = document.querySelector('#user-id');
 const $userPw = document.querySelector('#user-pw');
-const $userPwCheck = document.querySelector('#user-pw-check');
-const $userNickname = document.querySelector('#user-nickname');
+const $pwCheck = document.querySelector('#user-pw-check');
+const $nickname = document.querySelector('#user-nickname');
+const $idSame = document.querySelector('.alert-id');
+const $idReg = document.querySelector('.alert-idReg');
+const $pwReg = document.querySelector('.alert-pwReg');
+const $pwSame = document.querySelector('.alert-pw');
+const $nameReg = document.querySelector('.alert-name');
 
-// function
-const fetchUserAll = async () => {
+const fetchUsers = async () => {
   const data = await fetch('/users');
-  const res = await data.json();
-  console.log(res);
+  const users = await data.json();
+  console.log(users);
+
+  console.log(users.map(user => user.id));
+  const userId = users.map(user => user.id);
+
+  $userId.addEventListener('focusout', () => {
+    if (signReg.test($userId.value)) {
+      $idReg.style.display = 'none';
+      if (userId.includes($userId.value)) {
+        console.log($userId.value);
+
+        $idSame.style.display = 'block';
+      } else if (userId.includes($userId) === false) {
+        $idSame.style.display = 'none';
+      }
+    } else if (signReg.test($userId.value) === false) {
+      $idSame.style.display = 'none';
+      $idReg.style.display = 'block';
+    }
+    console.log(userId.includes($userId.value));
+  });
 };
 
-const fetchSingUp = async () => {
-  try {
-    await fetch('/users', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        id: $userId.value,
-        password: $userPw.value,
-        nickName: $userNickname.value
-      })
-    });
-  } catch (e) {
-    console.error(e);
+window.onload = fetchUsers;
+
+$userPw.addEventListener('focusout', () => {
+  if (signReg.test($userPw.value)) {
+    $pwReg.style.display = 'none'
+    console.log('none');
+
+  } else if (signReg.test($userPw.value) === false) {
+    $pwReg.style.display = 'block';
+    console.log($userPw.value);
+
+    console.log('block');
   }
-};
 
-// Event Binding
-window.onload = fetchUserAll;
+});
 
-$userId.addEventListener('focusout', e => {
-  console.log(e.target.value);
+$pwCheck.addEventListener('focusout', () => {
+  if ($userPw.value === $pwCheck.value) {
+    $pwSame.style.display = 'none';
+  } else if ($userPw.value !== $pwCheck.value) {
+    $pwSame.style.display = 'block';
+  }
+  if (!$pwCheck.value) {
+    console.log($pwCheck.value);
+
+    $pwSame.style.display = 'none';
+  }
+})
+
+$nickname.addEventListener('keyup', () => {
+  if (nameReg.test($nickname.value) === false) {
+    console.log($nickname.value);
+
+    $nameReg.style.display = 'block';
+  } else if (nameReg.test($nickname.value) === true) {
+    $nameReg.style.display = 'none';
+  }
 });
