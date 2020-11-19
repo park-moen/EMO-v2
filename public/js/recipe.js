c375a3b05118c72e051fa41652388d03b8ebb681
park moen <pmj6516@naver.com>
2020-11-19 23:45:47 +0900

    오늘이 마지막입니다!

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,5 +1,7 @@
 // status
 let foodList = [];
+let changeLocal = [];
+let memoState = [];
 // DOMs
 const $backBtn = document.querySelector('.back-btn');
 const $mainImage = document.querySelector('.main-image > img');
@@ -31,7 +33,19 @@ const getQueryString = (url) => {
 };
 
 const getLocation = () => JSON.parse(window.sessionStorage.getItem('ingredientes'));
-console.log(getLocation());
+
+// const setLocation = () => window.sessionStorage.setItem('ingredientes', JSON.stringify(changeLocal));
+
+// const setLocal = target => {
+//   changeLocal = getLocation();
+//   changeLocal = [...changeLocal, target];
+//   setLocation();
+// };
+
+// const getMemoId = res => {
+//   console.log(res);
+//   return res.length ? Math.max(res.map(elem => elem.id.substring(9)) + 1) : 1;
+// };
 
 const filterData = ingredient => {
   const data = ingredient.filter((_, i) => !getLocation().includes(ingredient[i]));
@@ -49,29 +63,36 @@ const fetchFoodList = async () => {
     $foodName.textContent = foodList.name;
     $lastSpan.textContent = foodList.difficulty;
     $mainImage.setAttribute('src', `${foodList.img}`);
-    render(res);
 
+    render(res);
     filterData(res.ingredient);
   } catch (e) {
     console.error(`error: ${e}`);
   }
 };
-const moveMemo = async content => {
-  try {
-    const data = await fetch('/cartmemos');
-    const res = await data.json();
-    const getMemoId = () => (res.length
-      ? Math.max(...res.map(elem => elem.id.substring(9))) + 1 : 1);
 
-    await fetch('/cartmemos', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id: `cart-item${getMemoId()}`, content, completed: false })
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
+// const fetchStuffList = async target => {
+//   try {
+//     const ReadData = await fetch('/cartmemos');
+//     const res = await ReadData.json();
+//     const dataInfo = res.map(elem => elem.content);
+//     let contents = '';
+//     dataInfo.forEach(moveItem => {
+//       if (moveItem === target.textContent) return;
+//       contents = target.textContent;
+//     });
+
+//     const dataId = res.length ? Math.max(...res.map(data => data.id.substring(9))) + 1 : 1;
+//     console.log(dataId);
+//     const createData = await fetch('/cartmemos', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({ id: `cart-item${dataId}`, content: contents, completed: false })
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 // Event Binding
 window.onload = () => {
@@ -82,9 +103,25 @@ $backBtn.onclick = () => {
   window.location.assign('cuisine.html');
 };
 
-$stuffList.onclick = ({ target }) => {
+$stuffList.onclick = async ({ target }) => {
   if (!target.matches('.stuff-list > li')) return;
   target.classList.remove('not-stuff');
-  moveMemo(target.textContent);
-  console.log(getLocation());
+  console.log(target.textContent);
+  const ReadData = await fetch(`/cartmemos?content=\\b${target.textContent}\\b`);
+  const res = await ReadData.json();
+  console.log(res);
+  // const dataInfo = res.map(elem => elem.content);
+  // console.log(dataInfo);
+  // memoState.push(target.textContent);
+  // memoState = [...new Set(memoState)];
+
+  // const dataId = res.length ? Math.max(...res.map(data => data.id.substring(9))) + 1 : 1;
+
+  // memoState.forEach(async item => {
+  //   const createData = await fetch('/cartmemos', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ id: `cart-item${dataId}`, content: item, completed: false })
+  //   });
+  // });
 };
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,36 +1,11 @@
 // DOMs
-const $form = document.querySelector('form');
+const $signupForm = document.querySelector('.signup-form');
 const $userId = document.querySelector('#user-id');
 const $userPw = document.querySelector('#user-pw');
 const $userPwCheck = document.querySelector('#user-pw-check');
 const $userNickname = document.querySelector('#user-nickname');
+const $userSignup = document.querySelector('.sign-up');
 
-// function
-const fetchUserAll = async () => {
-  const data = await fetch('/users');
-  const res = await data.json();
-  console.log(res);
+$signupForm.onclick = () => {
+  
 };
-
-const fetchSingUp = async () => {
-  try {
-    await fetch('/users', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        id: $userId.value,
-        password: $userPw.value,
-        nickName: $userNickname.value
-      })
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-// Event Binding
-window.onload = fetchUserAll;
-
-$userId.addEventListener('focusout', e => {
-  console.log(e.target.value);
-});
