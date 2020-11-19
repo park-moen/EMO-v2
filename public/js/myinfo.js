79fd11be6731cfca4e14b89ba86c844862110666
dunamisyoung <pksd5693@gmail.com>
2020-11-20 01:52:04 +0900

    fix ; 회원정보 기능 일부구현

--- public/js/myinfo.js
+++ public/js/myinfo.js
@@ -1,20 +1,31 @@
 let users = '';
 const $myName = document.querySelector('.my-name');
 
+// 닉네임으로 렌더하기
 const fetchUsers = async () => {
   try {
-    const res = await fetch(`/users/${guestInfoId}`);
+    const getLoginData = sessionStorage.getItem('login');
+    const loginResponse = JSON.parse(getLoginData);
+    const loginNick = loginResponse.id;
+
+    const res = await fetch(`/users/${loginNick}`);
     users = await res.json();
-    render()
+    render();
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 const render = () => {
   $myName.textContent = users.nickName;
-}
-
-
+};
 
 window.onload = fetchUsers;
+
+// const getQueryString = (url) => {
+//   const str = url.split('=');
+//   const qureyId = str[str.length - 1];
+//   return qureyId;
+// };
+
+// console.log(getQueryString(window.location.href));
